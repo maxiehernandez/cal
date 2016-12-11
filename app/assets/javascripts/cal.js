b813ab152b7fdc3fc3eb2d0d446476999b39419f
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-12-11 15:06:45 -0500

    Saving calculations to browser session

--- app/assets/javascripts/cal.js
+++ app/assets/javascripts/cal.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
+  for (var key in sessionStorage){
+   $('.last-calculations').append(
+    '<p>' +
+      sessionStorage.getItem(key) +
+    '</p>')
+  }
   $(document).on('click', ".box", function(){
+    $(".final-answer").empty()
     var type = $(this).data().type
     if (type !== "=") {
       $('.display-calculation').append(type)
@@ -19,9 +26,15 @@ $( document ).ready(function() {
           }
         })
         .done(function(data){
+          sessionStorage.setItem(generateRandomString(), $('.display-calculation').text());
           $(".final-answer").html(data.result);
+          $('.display-calculation').empty();
         })
         break;
     }
   })
 });
+
+function generateRandomString(){
+  return Math.random().toString(36).substring(8);
+}
